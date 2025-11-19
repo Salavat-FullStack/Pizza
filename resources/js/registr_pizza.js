074d79b5c9048035b53a2d43bdfe742cb1ec55f4
Salavat-FullStack <webfuldeveloper@gmail.com>
2025-11-19 17:40:49 +0500

    работа над очисткой корзины

--- resources/js/registr_pizza.js
+++ resources/js/registr_pizza.js
@@ -1,6 +1,34 @@
+import { allShope } from './shope_function.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('window.registerPizzaData ', window.registerPizzaData);
     const data = window.registerPizzaData;
+
+    const btnClear = document.querySelector('.btn_clear_basket');
+
+    btnClear.addEventListener('click',()=>{
+        fetch('http://127.0.0.1:8000/api/clear-basket',{
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            credentials: 'include',
+        })
+        .then(async response => {
+            const data = await response.json();
+
+            if (!response.ok) { // если статус не 200-299
+                console.error('Ошибка сервера:', data);
+                alert(data.message || 'Ошибка регистрации');
+                return;
+            }
+
+            allShope();
+
+            console.log(data);
+        })
+    });
 
     // let finelPrice = 0;
 
